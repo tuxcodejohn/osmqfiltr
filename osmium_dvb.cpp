d3fab2c7d1f781a2d5ae294c232de0422916ed79
john stone <john@tuxcode.org>
2013-04-20 01:05:53 +0200

    aufhübschung

--- osmium_dvb.cpp
+++ osmium_dvb.cpp
@@ -1,9 +1,14 @@
 /*
- * =====================================================================================
+ * ===========================================================================
  *
  *       Filename:  osmium_dvb.cpp
  *
- *    Description:  
+ *    Description:  stdin: osmpbf
+ *    	            stdout: filtered output
+ *
+ *    	            based on simplyfied osmium example. 
+ *    	            osmium is provided in git submodule.
+ *    	            see documentation there
  *
  *        Version:  1.0
  *        Created:  19.04.2013 23:26:52
@@ -11,48 +16,46 @@
  *       Compiler:  gcc
  *
  *         Author:  _john (), 
- *   Organization:  
+ *   Organization:  <<</>>
  *
- * =====================================================================================
+ * ==========================================================================
  */
 #include <iostream>
 #include <cstring>
 
 #define OSMIUM_WITH_PBF_INPUT
 #include <osmium.hpp>
-//#include <osmium/geometry/point.hpp>
- 
 
-
-class MyFilterHandler :public Osmium::Handler::Base {
+class 
+MyFilterHandler :public Osmium::Handler::Base 
+{
 
 	public:
-		MyFilterHandler() : Osmium::Handler::Base(){
+	MyFilterHandler() : Osmium::Handler::Base(){ }
+
+	void 
+	node( const boost::shared_ptr<Osmium::OSM::Node const>& node) 
+	{
+		const char* op = node->tags().get_value_by_key("operator");
+		if(  op && 
+		     !strncmp(op ,"DVB",3) 
+		     //  &&  !strcmp(node->tags().get_value_by_key("public_transport"),"stop_position") 
+		    ) { std::cout << "ha!: " 
+				  << node->id() 
+				  << "[" 
+				  << node->tags().get_value_by_key("name") 
+				  << "] \n";
 		}
-
-		void node( const boost::shared_ptr<Osmium::OSM::Node const>& node) 
-		{
-			const char* op = node->tags().get_value_by_key("operator");
-			if(  op && 
-			     !strncmp(op ,"DVB",3) 
-			 //  &&  !strcmp(node->tags().get_value_by_key("public_transport"),"stop_position") 
-			     ){
-
-
-				std::cout << "ha!: " << node->id() <<"[" << node->tags().get_value_by_key("name") << "] \n";
-
-			}
-		}
+	}
 };
 
 
 
-int main(int, char *argv[]) {
+int main(int, char *argv[]) 
+{
 	Osmium::OSMFile infile(argv[1]);
 	MyFilterHandler dvbfilterhandler;
 	Osmium::Input::read(infile, dvbfilterhandler);
-
 	google::protobuf::ShutdownProtobufLibrary();
-
 }
 
