6c88389577981d37ab580f3a3078588b64eac985
john stone <john@tuxcode.org>
2013-04-20 00:48:48 +0200

    put some initial testing c++ code in

---
+++ Makefile
@@ -0,0 +1,35 @@
+#------------------------------------------------------------------------------
+#
+#  Makefile for Osmium testing...
+
+#CXX = g++
+CXX = clang++
+
+CXXFLAGS = -g
+#CXXFLAGS = -O3 -g
+
+CXXFLAGS += -Wall -Wextra -Wdisabled-optimization -pedantic -Wctor-dtor-privacy -Wnon-virtual-dtor -Woverloaded-virtual -Wsign-promo -Wno-long-long
+
+CXXFLAGS += -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
+CXXFLAGS += -I./osmium/include/
+
+# remove this if you do not want debugging to be compiled in
+CXXFLAGS += -DOSMIUM_WITH_DEBUG
+
+LIB_EXPAT  = -lexpat
+LIB_PBF    = -lz -lpthread -lprotobuf-lite -losmpbf
+
+LDFLAGS = $(LIB_EXPAT) $(LIB_PBF)
+
+PROGRAMS = osmium_dvb
+
+
+.PHONY: all clean
+
+all: $(PROGRAMS)
+
+osmium_dvb: osmium_dvb.cpp
+	$(CXX) $(CXXFLAGS)  -o $@ $< $(LDFLAGS) 
+clean:
+	rm -f *.o core $(PROGRAMS)
+
---
+++ osmium_dvb.cpp
@@ -0,0 +1,58 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  osmium_dvb.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  19.04.2013 23:26:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  _john (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <cstring>
+
+#define OSMIUM_WITH_PBF_INPUT
+#include <osmium.hpp>
+//#include <osmium/geometry/point.hpp>
+ 
+
+
+class MyFilterHandler :public Osmium::Handler::Base {
+
+	public:
+		MyFilterHandler() : Osmium::Handler::Base(){
+		}
+
+		void node( const boost::shared_ptr<Osmium::OSM::Node const>& node) 
+		{
+			const char* op = node->tags().get_value_by_key("operator");
+			if(  op && 
+			     !strncmp(op ,"DVB",3) 
+			 //  &&  !strcmp(node->tags().get_value_by_key("public_transport"),"stop_position") 
+			     ){
+
+
+				std::cout << "ha!: " << node->id() <<"[" << node->tags().get_value_by_key("name") << "] \n";
+
+			}
+		}
+};
+
+
+
+int main(int, char *argv[]) {
+	Osmium::OSMFile infile(argv[1]);
+	MyFilterHandler dvbfilterhandler;
+	Osmium::Input::read(infile, dvbfilterhandler);
+
+	google::protobuf::ShutdownProtobufLibrary();
+
+}
+
